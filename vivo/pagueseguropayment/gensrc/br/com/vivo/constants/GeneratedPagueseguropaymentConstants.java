8eaff9462b112b28d2beca0dc07315a92d7241e8
Daniel Alexandre Diniz <dalexadi@emeal.nttdata.com>
2022-10-05 10:51:39 -0300

    adicionando info de properties e pacote de dto

---
+++ vivo/pagueseguropayment/gensrc/br/com/vivo/constants/GeneratedPagueseguropaymentConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 5 de out de 2022 09:20:26                   ---
+ * ----------------------------------------------------------------
+ */
+package br.com.vivo.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedPagueseguropaymentConstants
+{
+	public static final String EXTENSIONNAME = "pagueseguropayment";
+	
+	protected GeneratedPagueseguropaymentConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
